8e16709b83146d24fa6d62e202c5227e0cff16c5
Zak <zachary.strassberg@abbvie.com>
2020-12-02 13:28:35 -0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { sample } from "lodash";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useList } from "react-use";
 import data from "./service/data";
 import Spinner from "./Spinner";
@@ -23,7 +23,7 @@ const radius = 500;
 const App = () => {
   const [list, { push }] = useList<string>(data);
   const [power, setPower] = useState(0);
-  const [acc, setAcc] = useState(0);
+  const acc = useRef(0);
   const [props, set] = useSpring(() => ({
     transform: "rotate(0deg)",
     immediate: false,
@@ -32,28 +32,19 @@ const App = () => {
   useEffect(() => {
     const config = { mass: 50, tension: 200, friction: 200, precision: 0.001 };
     set({
-      from: { transform: `rotate(${map(acc, 0, 100, 0, 1700)}deg)` },
-      transform: `rotate(${map(acc + power, 0, 100, 0, 1700)}deg)`,
+      from: { transform: `rotate(${map(acc.current, 0, 100, 0, 1700)}deg)` },
+      transform: `rotate(${map(acc.current + power, 0, 100, 0, 1700)}deg)`,
       immediate: false,
       config,
     });
-    setAcc((a) => a + power);
-    // If acc is in here it never stops
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    acc.current = acc.current + power;
   }, [power, set]);
 
   return (
     <div>
       <button
         onClick={() => {
-          push(sample(data) ?? "");
-        }}
-      >
-        draw
-      </button>
-      <button
-        onClick={() => {
-          setPower((p) => p + 10);
+          setPower((p) => p + 2);
         }}
       >
         spin
